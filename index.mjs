ebfcd7dde009f11ccb3ebf01dad10a9a8dc7b6ae
Luke Brody <lukebrody@pavonine.co>
2024-09-02 10:51:57 -0700

    Sort by top two songs in group

--- index.mjs
+++ index.mjs
@@ -20,8 +20,18 @@ for (const track of tracks) {
     }
 }
 
-const multiple = Array.from(byName.values()).filter(value => value.length > 1).map(songs => songs.map(({ Name, Artist }) => ({ Name, Artist })));
+const multiple = Array.from(byName.values())
+    .filter(value => value.length > 1)
+    .map(songs => 
+        songs
+        .map(({ Name, Artist, 'Play Count': playCount }) => ({ Name, Artist, 'Play Count': playCount ?? 0 }))
+        .sort((a, b) => b['Play Count'] - a['Play Count'])
+    );
 
-console.log(`${multiple.length} Sets of duplicates`)
+console.log(`${multiple.length} Sets of duplicates`);
+
+const averageOfTopTwo = (sortedSongs) => (sortedSongs[0]['Play Count'] + sortedSongs[1]['Play Count']) / 2
+
+multiple.sort((set1, set2) => averageOfTopTwo(set2) - averageOfTopTwo(set1))
 
 fs.writeFileSync('output.json', JSON.stringify(multiple, null, 2))
