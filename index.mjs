1d9721b0b9384fcf0829fb57baaba94d89c1b9f8
Luke Brody <lukebrody@pavonine.co>
2024-09-02 09:52:58 -0700

    Handle features correctly

--- index.mjs
+++ index.mjs
@@ -10,16 +10,18 @@ const tracks = Object.values(library['Tracks']);
 const byName = new Map();
 
 for (const track of tracks) {
-    const name = track['Name'];
-    if (!byName.has(name)) {
-        byName.set(name, []);
+    const nameKey = track['Name'].replaceAll(/\(.*\)/g, '').trim();
+    if (!byName.has(nameKey)) {
+        byName.set(nameKey, []);
     }
-    const sameName = byName.get(name);
+    const sameName = byName.get(nameKey);
     if (!sameName.some(otherTrack => otherTrack['Artist'] === track['Artist'])) {
         sameName.push(track);
     }
 }
 
-const multiple = Array.from(byName.values()).filter(value => value.length > 1).map(songs => songs.map(({ Name, Artist }) => ({ Name, Artist }))); 
+const multiple = Array.from(byName.values()).filter(value => value.length > 1).map(songs => songs.map(({ Name, Artist }) => ({ Name, Artist })));
+
+console.log(`${multiple.length} Sets of duplicates`)
 
 fs.writeFileSync('output.json', JSON.stringify(multiple, null, 2))
